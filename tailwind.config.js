da762a326bc2342744a54e5579a2fff78ee454a1
LionelMugisha <lionelmugisha2@gmail.com>
2021-10-01 00:37:24 +0200

    initialise the commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        notdark: '#343a40',
+        'white-dark': '#495057',
+        green: '#59981A',
+        'green-yellow': '#ECF87F',
+        'green-lime': '#81B622',
+        'green-olive': '#3D550C',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
